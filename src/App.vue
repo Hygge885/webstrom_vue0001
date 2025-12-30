01061a48d6ae4b97c05f051e9b528da2da6a9ff8
Hygge <354646961@qq.com>
2025-12-30 16:19:54 +0800

    1.0.1(写入App.vue)

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+const msg = "Hello World!"
+const day = 30
+const b = false
+const msg2 = "倒过来读的是谁zzhu"
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  {{ msg }}
+  {{ day + 1 }}
+  {{ b ? 'yes' : 'no' }}
+  {{ msg2.split('').reverse().join('') }}
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
