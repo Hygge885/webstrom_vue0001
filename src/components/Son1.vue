64d32d6321396230842d59710636b5b7d4c0d127
cross-sunny <973564004@qq.com>
2025-12-30 17:20:32 +0800

    1.1(修改Son1,Son2,App.vue)

---
+++ src/components/Son1.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h3>我是 Son1 组件</h3>
+    <p>来自父组件的数据：{{ msg }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  msg: String
+})
+</script>
---
+++ src/components/Son2.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h3>我是 Son2 组件</h3>
+    <p>我接收了一个数字：{{ count }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  count: Number
+})
+</script>
